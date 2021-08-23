118e50a8232f85a6dcf61d71223ef83396b23701
itzhackB <yzikbynesai@gmail.com>
2021-08-23 13:47:36 +0300

    side bar tabs & role

--- client/src/Routing/MyCourseRouting.js
+++ client/src/Routing/MyCourseRouting.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Route,
     Switch
 } from "react-router-dom";
 import CourseSchedule from '../Components/Pages/CourseSchedule/CourseScheduleComponent';
+import Tab from 'react-bootstrap/Tab'
+import Tabs from 'react-bootstrap/Tabs'
 
 import DailySchedule from '../Components/Pages/DailySchedule/DailyScheduleComponent';
 import StudentGradesComponent from '../Components/Pages/Grades/StudentGradesComponent'
@@ -11,15 +13,36 @@ import AdminGradesComponent from '../Components/Pages/Grades/AdminGradesComponen
 import Syllabus from '../Components/Pages/Syllabus/SyllabusComponent'
 
 const MyCourseRouting = () => {
+    const [tabName, setTabName] = useState('daily-schedule');
     // temp role for switch component
     const role = "Admin"
     return (
-        <Switch>
-            <Route path="/my-course/daily-schedule" component={DailySchedule} />
-            <Route path="/my-course/grades" component={role === 'Student' ? StudentGradesComponent : AdminGradesComponent} />
-            <Route path="/my-course/course-schedule" component={CourseSchedule} />
-            <Route path="/my-course/syllabus" component={Syllabus} />
-        </Switch>
+        <>
+            <Tabs
+                id="controlled-tab-example"
+                activeKey={tabName}
+                onSelect={(k) => setTabName(k)}
+                className="mb-3"
+                transition   
+            >
+                <Tab eventKey="daily-schedule" title="לוז יומי" >
+                    <DailySchedule />
+                </Tab>
+                <Tab eventKey="grades" title="ציונים">
+                    <StudentGradesComponent />
+                </Tab>
+                <Tab eventKey="course-schedule" title="לוז חודשי" >
+                    <CourseSchedule />
+                </Tab>
+                <Tab eventKey="syllabus" title="סילבוס" >
+                    <Syllabus />
+                </Tab>
+                <Tab eventKey="staff" title="סגל" disabled={role === "admin" ? false : true} >
+                    <AdminGradesComponent />
+                </Tab>
+            </Tabs>
+        </>
+
     )
 }
 export default MyCourseRouting;
